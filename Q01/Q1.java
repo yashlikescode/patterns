a8ae07bf5c3761f298931be220fe37d3f757bb9d
unknown <kmryashasvi@gmail.com>
2021-01-23 16:44:50 +0530

    fix:managed files

---
+++ Q01/Q1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Q1 {
+
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        int numberOfRows;
+
+        // the loop is used to check if the given number can make a pattern
+        while (true) {
+            System.out.print("Enter a number: ");
+            numberOfRows = scanner.nextInt();
+            if (numberOfRows > 0)
+                break;
+            else
+                System.out.println("Invalid number. Please try again.");
+        }
+
+        // outer loop is for the number of rows while the inner is for the number of * per row
+        for (int i = numberOfRows; i > 0; i--) {
+            for (int j = i; j > 0; j--)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+}
