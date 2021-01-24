ed1e1174f8bb312ec286f64aef8cb6530845c0f3
unknown <kmryashasvi@gmail.com>
2021-01-24 13:16:22 +0530

    fix:managed files into directories

--- Q03/Q3.py
+++ Q03/Q3.py
@@ -3,8 +3,6 @@
 x = int(input("Enter number of row for the pattern : "))
 i = 1
 while i <= x:
-    for j in range(x-i):
-        print(" ", end ="")
     for j in range(i):
         print('*', end ="") # for python 2.7.x write print('*'),
     print("")
--- Q09/Q9.py
+++ Q09/Q9.py
@@ -4,8 +4,17 @@ x = int(input("Enter number of row for the pattern : "))
 i = 1
 while i <= x:
     for j in range(x-i):
-        print(" ", end ="")
+        print("", end = " ")
+    for j in range(2*i-1):
+        print('*', end ="") # for python 2.7.x write print('*'),
+    print("")
+    i+=1
+
+i = 1
+while i <= x:
     for j in range(i):
+        print("", end = " ")
+    for j in range((x-i)*2-1):
         print('*', end ="") # for python 2.7.x write print('*'),
     print("")
     i+=1
