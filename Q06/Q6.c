4387eaa09ce1625b0e35e27f633e97852b8f9e2a
kmryashasvi <66861659+kmryashasvi@users.noreply.github.com>
2021-02-06 18:38:06 +0530

    Create Q6.c

---
+++ Q06/Q6.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main (void)
+{
+    printf("Enter Height: ");
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        for (int sp = 0; sp < n-1-i; sp++)
+        {
+            printf(" ");
+        }
+        for (int st = 0; st < 2*i+1; st++)
+        {
+            printf("*");
+        }
+        
+        printf("\n");
+        
+    }
+}
