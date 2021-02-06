f091f1c17f5a37aa0a44a8b0eae6b52d173e3236
kmryashasvi <66861659+kmryashasvi@users.noreply.github.com>
2021-02-06 13:07:35 +0530

    added Q1.c

---
+++ Q01/Q1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Enter height: ");
+    int height = 0;
+    scanf("%d", &height);
+    for (int i = 0; i < height; i++)
+    {
+        for  (int p = 0; p < height - i; p++)
+        {
+            printf("*");
+        }
+        ptintf("\n");
+    }
+}
