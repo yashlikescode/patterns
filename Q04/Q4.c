05f5c35832e5bbec0c34c5e3452fb7c920fa7de1
kmryashasvi <66861659+kmryashasvi@users.noreply.github.com>
2021-02-06 18:31:14 +0530

    Create Q4.c

---
+++ Q04/Q4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main (void)
+{
+    printf("Enter Height: ");
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        for (int sp = 0; sp < n-1-i; sp++)
+        {
+            printf(" ");
+        }
+        for (int st = 0; st < i+1; st++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
