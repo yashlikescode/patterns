65d2a6f686f7d47ac2407bd452476446dc407ef0
kmryashasvi <66861659+kmryashasvi@users.noreply.github.com>
2021-02-06 18:36:09 +0530

    Update and rename Q19 to Q19.c

---
+++ Q19.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main (void)
+{
+    printf("Enter Height: ");
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        for (int sp = 0; sp < n-1-i; sp++)
+        {
+            printf(" ");
+        }
+        for (int st = 0; st < i+1; st++)
+        {
+            printf("*");
+        }
+        printf("  ");
+        for (int st2 = 0; st2 < i+1; st2++)
+        {
+            printf("*");
+        }
+        printf("\n");
+        
+    }
+}
