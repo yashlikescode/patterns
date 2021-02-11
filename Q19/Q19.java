9aa127847cbe900af1f79ecaf69351cfb0007f9a
jpzs444 <75724780+jpzs444@users.noreply.github.com>
2021-02-11 19:58:11 +0800

    ✨ feat: add scanner and input checker

---
+++ Q19/Q19.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Q19 {
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        int numberOfRows;
+
+        // the loop is used to check if the number given can make a pattern
+        while (true) {
+            System.out.print("Enter a number: ");
+            numberOfRows = scanner.nextInt();
+            if (numberOfRows > 0)
+                break;
+            System.out.println("Invalid number. Please try again.");
+        }
+    }
+}
