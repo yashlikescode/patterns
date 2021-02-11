bad61db5c64b6b1c3be091d9039584cc047fdc56
jpzs444 <75724780+jpzs444@users.noreply.github.com>
2021-02-11 20:04:28 +0800

    feat: add the code that will print the Q19 pattern

--- Q19/Q19.java
+++ Q19/Q19.java
@@ -13,5 +13,18 @@ public class Q19 {
                 break;
             System.out.println("Invalid number. Please try again.");
         }
+        // outer loop is for the number of rows
+        // the 1st inner loop is for the number of initial spaces per row
+        // the 2nd and 3rd inner loop are for the number of asterisks per row
+        for (int i = numberOfRows; i > 0; i--) {
+            for (int j = i - 1; j > 0; j--)
+                System.out.print(" ");
+            for (int k = 1; k <= numberOfRows - i + 1; k++)
+                System.out.print("*");
+            System.out.print("  ");
+            for (int k = 1; k <= numberOfRows - i + 1; k++)
+                System.out.print("*");
+            System.out.println();
+        }
     }
 }
