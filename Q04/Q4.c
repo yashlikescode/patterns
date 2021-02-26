286b649eb7ce362646a0c180d7d545d95d03ad73
Atulya Singh <71915346+oreo-gif@users.noreply.github.com>
2021-02-26 09:35:54 +0530

    Update Q4.c

--- Q04/Q4.c
+++ Q04/Q4.c
@@ -2,21 +2,22 @@
 #include <string.h>
 #include <ctype.h>
 
+//for printing a pattern of '*' in a right angle triangle with hypotenuse towards the left
 int main (void)
 {
     printf("Enter Height: ");
     int n;
     scanf("%d", &n);
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)//for the height of the triangle
     {
-        for (int sp = 0; sp < n-1-i; sp++)
+        for (int sp = 0; sp < n-1-i; sp++)//for printing the spacing in making the hypotenuse
         {
             printf(" ");
         }
-        for (int st = 0; st < i+1; st++)
+        for (int st = 0; st < i+1; st++)//for printing the line of '*'
         {
             printf("*");
         }
-        printf("\n");
+        printf("\n");//mandatory for going to the next line
     }
 }
