94ada33b86b7cbd45870ca10f8c38c12524fc73e
Atulya Singh <71915346+oreo-gif@users.noreply.github.com>
2021-02-28 16:55:23 +0530

    Update Q5.c

--- Q05/Q5.c
+++ Q05/Q5.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-
+//printing a pattern of inverted isosceles triangle using '*'
 int main (void)
 {
     printf("Enter Height: ");
     int n;
     scanf("%d", &n);
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)//for height of triangle
     {
-        for (int sp = 0; sp < i; sp++ )
+        for (int sp = 0; sp < i; sp++ )//for the required spacing
         {
             printf(" ");
         }
-        for (int st = 0; st < 2*n-1-i*2; st++)
+        for (int st = 0; st < 2*n-1-i*2; st++)//for printing the required number of '*' in each line
         {
             printf("*");
         }
