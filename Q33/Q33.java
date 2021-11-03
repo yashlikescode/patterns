c757e54a6562a82289da96d3d97f3cb4140e5973
barismutan <barismutan@gmail.com>
2021-11-03 19:38:00 +0300

    added Q33 directory and Q33.Java

---
+++ Q33/Q33.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Q33 {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int len = s.nextInt();
+        int k=1;
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                if (i%2==0){
+                    System.out.print(i*len+j+1+" ");
+
+                }else{
+                    System.out.print((i+1)*len-j+" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
