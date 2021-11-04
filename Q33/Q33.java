5b77f7a1d9bf4f6fe8b50c2ec10b0c733052b3c7
Yashasvi <66861659+stlyash@users.noreply.github.com>
2021-11-04 21:04:39 +0530

    Update Q33.java

--- Q33/Q33.java
+++ Q33/Q33.java
@@ -4,6 +4,7 @@ public class Q33 {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        System.out.print("Enter Number of Rows: ");
         int len = s.nextInt();
         int k=1;
         for (int i = 0; i < len; i++) {
